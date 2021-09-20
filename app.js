a70ee2f982d7d422f5a9659b7030906858ba0eb4
Rommel Dominguez <bm-2cu8dmto5ga5pf2xkvapdrqcpwtzybsxpw@riseup.net>
2021-09-20 11:24:49 +0800

    simple and useless JavaScript code

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-
+function fOne()
+{    
+        return "I am Function The First"
+}                                             return "I am Function One"        }
+function fTwo()
+{    
+        return "I am Function Number Two"
+}                                             return "I am Function Two"        }
+console.log(fOne())
+console.log(fTwo())
