f69407b8ea779f83cc2b63a90b452bec7f54f3f8
Rommel Dominguez <bm-2cu8dmto5ga5pf2xkvapdrqcpwtzybsxpw@riseup.net>
2021-09-23 12:09:59 +0800

    added more subtle changes

--- app.js
+++ app.js
@@ -21,9 +21,40 @@ function fSix()
 {
 	return "I am Function Number Six"
 }
+function fSeven()
+{    
+        return "I am Function Number Seven"
+}
+function fEight()
+{    
+        return "I am Function Number Eight"
+}
+function fNine()
+{
+        return "I am Function Number Nine"
+}
+function fTen()
+{
+        return "I am Function Number Ten"
+}
+function fEleven()
+{
+	return "I am Function Number Eleven"
+}
+function fTwelve()
+{
+	return "I am Function Number Twelve"
+}
 console.log(fOne())
 console.log(fTwo())
 console.log(fThree())
 console.log(fFour())
 console.log(fFive())
 console.log(fSix())
+console.log(fSeven())
+console.log(fEight())
+console.log(fNine())
+console.log(fTen())
+console.log(fEleven())
+console.log(fTwelve())
+
